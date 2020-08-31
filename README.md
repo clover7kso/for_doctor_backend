8846375675ccc4b7018298b8fe9e8af5895a456f
woony1 <woony@luxrobo.com>
2020-08-31 17:36:09 +0900

    Working on Database

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# doctor_sharp
+# doctor_sharp_backend
+
 This is APP for medical dotor's community and medical product's advertisment
---
+++ src/server.js
@@ -0,0 +1,27 @@
+import "./env";
+import { GraphQLServer, PubSub } from "graphql-yoga";
+import logger from "morgan";
+import schema from "./schema";
+import "./passport";
+import { authenticateJwt } from "./passport";
+import { isAuthenticated } from "./middlewares";
+import { uploadMiddleware, uploadController } from "./upload";
+import { PrismaClient } from "@prisma/client";
+
+const PORT = process.env.PORT || 4000;
+
+const prisma = new PrismaClient();
+const pubsub = new PubSub();
+
+const server = new GraphQLServer({
+  schema,
+  context: ({ request }) => ({ request, isAuthenticated, prisma, pubsub }),
+});
+
+server.express.use(logger("dev"));
+server.express.use(authenticateJwt);
+server.express.post("/api/upload", uploadMiddleware, uploadController);
+
+server.start({ port: PORT }, () =>
+  console.log(`✅ Server running on http://localhost:${PORT}`)
+);
