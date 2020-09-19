85e65298c6db81486910fe06d3664c7c4912474d
woony <clover7kso@naver.com>
2020-09-19 22:44:05 +0900

    Working on postOne

--- src/api/Post/Post.js
+++ src/api/Post/Post.js
@@ -1,3 +1,18 @@
+import { timeFromToday } from "../../utils";
+
 export default {
-  Post: {},
+  Post: {
+    userName: async ({ id }, _, { prisma }) => {
+      const user = await prisma.post.findOne({ where: { id } }).user();
+      return user.name;
+    },
+    commentCount: async ({ id }, _, { prisma }) => {
+      const comments = await prisma.post.findOne({ where: { id } }).comments();
+      return comments.length;
+    },
+    timeFromToday: async ({ id }, _, { prisma }) => {
+      const item = await prisma.post.findOne({ where: { id } });
+      return timeFromToday(item.createdAt);
+    },
+  },
 };
--- src/api/Post/postTop/postTop.js
+++ src/api/Post/postTop/postTop.js
@@ -2,13 +2,27 @@ export default {
   Query: {
     postTop: async (_, args, { prisma }) => {
       const {} = args;
+      const take = 3;
+      const categories = [
+        "자유게시판",
+        "학술정보공유게시판",
+        "동호회게시판",
+        "중고거래게시판",
+      ];
+      var posts = [];
+      for (var i = 0; i < categories.length; i++) {
+        const adding = await prisma.post.findMany({
+          take: take,
+          where: {
+            category: categories[i],
+          },
+          orderBy: {
+            todayViews: "desc",
+          },
+        });
+        posts = [...posts, ...adding];
+      }
 
-      const posts = await prisma.post.findMany({
-        distinct: ["category"],
-        orderBy: {
-          todayViews: "desc",
-        },
-      });
       return posts;
     },
   },
