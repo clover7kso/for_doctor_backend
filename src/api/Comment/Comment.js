98b4f6e9e9e8b520709b6ea79acb111a4af77ba0
woony <clover7kso@naver.com>
2020-09-22 21:01:29 +0900

    Finish with Load Comment

--- src/api/Comment/Comment.js
+++ src/api/Comment/Comment.js
@@ -1,3 +1,18 @@
+import { timeFromToday } from "../../utils";
+
 export default {
-  Comment: {},
+  Comment: {
+    userName: async ({ id }, _, { prisma }) => {
+      const user = await prisma.comment.findOne({ where: { id } }).user();
+      return user.name;
+    },
+    userAvatar: async ({ id }, _, { prisma }) => {
+      const user = await prisma.comment.findOne({ where: { id } }).user();
+      return user.avatar;
+    },
+    timeFromToday: async ({ id }, _, { prisma }) => {
+      const item = await prisma.comment.findOne({ where: { id } });
+      return timeFromToday(item.createdAt);
+    },
+  },
 };
--- src/api/Post/Post.js
+++ src/api/Post/Post.js
@@ -6,6 +6,10 @@ export default {
       const user = await prisma.post.findOne({ where: { id } }).user();
       return user.name;
     },
+    userAvatar: async ({ id }, _, { prisma }) => {
+      const user = await prisma.post.findOne({ where: { id } }).user();
+      return user.avatar;
+    },
     commentCount: async ({ id }, _, { prisma }) => {
       const comments = await prisma.post.findOne({ where: { id } }).comments();
       return comments.length;
@@ -13,6 +17,13 @@ export default {
     timeFromToday: async ({ id }, _, { prisma }) => {
       const item = await prisma.post.findOne({ where: { id } });
       return timeFromToday(item.createdAt);
+    },
+    comments: async ({ id }, _, { prisma }) => {
+      const items = await prisma.comment.findMany({
+        where: { postId: id },
+        orderBy: [{ createdAt: "desc" }],
+      });
+      return items;
     },
   },
 };
