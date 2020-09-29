38bb4732b15717f52e58804aac58787682fea381
woony <clover7kso@naver.com>
2020-09-29 19:18:04 +0900

    Finish with profile Update

--- src/api/Profile/avatarEdit/avatarEdit.js
+++ src/api/Profile/avatarEdit/avatarEdit.js
@@ -1,4 +1,4 @@
-import { generateSecret, sendSecretMail } from "../../../utils";
+import aws from "aws-sdk";
 
 export default {
   Mutation: {
@@ -6,12 +6,32 @@ export default {
       const user = request.user;
       const { avatar } = args;
 
+      const s3 = new aws.S3({
+        accessKeyId: process.env.AWS_KEY,
+        secretAccessKey: process.env.AWS_SECRET,
+        region: "ap-northeast-2",
+      });
+      if (user.avatar !== null) {
+        const key = user.avatar.replace(
+          "https://for-doctor.s3.ap-northeast-2.amazonaws.com/",
+          ""
+        );
+        s3.deleteObject({ Bucket: "for-doctor", Key: key }, function (
+          err,
+          data
+        ) {
+          console.log(err);
+          console.log(data);
+        });
+      }
+
       await prisma.user.update({
         where: { id: user.id },
         data: {
           avatar,
         },
       });
+      return true;
     },
   },
 };
