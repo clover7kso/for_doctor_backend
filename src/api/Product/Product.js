ceef5b6a6a2c6e985dcce4eb0fde57be5f2a08d3
woony <clover7kso@naver.com>
2020-11-01 01:24:27 +0900

    Adding Sending Message

--- src/api/Product/Product.js
+++ src/api/Product/Product.js
@@ -21,5 +21,31 @@ export default {
       const marketer = await prisma.product.findOne({ where: { id } }).Marketer()
       return marketer.company_name;
     },
+    marketerName: async ({ id }, _, { prisma }) => {
+      const product = await prisma.product.findOne({ where: { id } })
+      const user = await prisma.user.findOne({ where: { id: product.marketerId } })
+      return user.name;
+    },
+    chatRoomId: async ({ id }, _, { prisma, request }) => {
+      const product = await prisma.product.findOne({ where: { id } })
+      const userId = await request.user.id
+      const rooms = await prisma.room.findMany({
+        where: {
+          AND:[{
+            participants: {
+              some: {
+                id: userId
+              }
+            },
+            participants: {
+              some: {
+                id: product.marketerId
+              }
+            },
+          }]
+        }
+      });
+      return rooms.length>0?rooms[0].id:null;
+    },
   },
 };
