ee85203b5a9b622fd0294dde8a832b7789037935
woony <clover7kso@naver.com>
2020-11-15 00:44:59 +0900

    Update backend

--- src/api/Post/postSubCategory/postSubCategory.js
+++ src/api/Post/postSubCategory/postSubCategory.js
@@ -1,9 +1,9 @@
 export default {
   Query: {
-    postSubCategory: async (_, args, {}) => {
+    postSubCategory: async (_, args, {request}) => {
       const {} = args;
 
-      var result = ['자유게시판','학술정보공유게시판','중고거래게시판'];
+      var result = request.user.role==="DOCTOR"?['자유게시판','학술정보공유게시판','중고거래게시판']:['자유게시판','학술정보공유게시판'];
       
       return result;
     },
